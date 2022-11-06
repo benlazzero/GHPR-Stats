5dc99e1e3023e56dd2fcafa38de8e19f0370da2a
benlazzero <85390423+benlazzero@users.noreply.github.com>
2022-11-06 00:43:11 -0700

    changed abstractions -> class

---
+++ closedPr.js
@@ -0,0 +1,151 @@
+class ClosedPr {
+  constructor(prArr, urlArr, userAdmins) {
+    this.userAdmins = this.#parseLogins(userAdmins)
+    this.urls = urlArr
+    this.pulls = prArr
+    this.repoOwner = this.#getOwnerUsername()
+    this.mergedPulls = this.#getMergedPulls()
+    this.mergeDates = this.#getPrMergeDates()
+    this.adminsJson = this.#getAdminObjects()
+  }
+  
+  /**
+   * Uses user input from the form to return an array of usernames of reviewers
+   * @param {string} userAdmins - The string from form input for adding reviewers to search for
+   */
+  #parseLogins = (userAdmins) => {
+    const usernames = userAdmins
+    if (usernames.length > 100 || usernames.length == 0) {
+      return ''
+    }
+    const noSpaces = usernames.replace(/\s/g, '')
+    const arrOfUsernames = noSpaces.split(',')
+    for (let i = 0; i < arrOfUsernames.length; i++) {
+      if (arrOfUsernames[i].length > 39) {
+        arrOfUsernames.splice(i, 1)
+      } 
+    }
+    return arrOfUsernames
+  }
+  
+  // Uses the url array to return the username for the repo 
+  #getOwnerUsername = () => {
+    const repoUrl = new URL(this.urls[0])
+    let paths = repoUrl.pathname.split('/')
+    return paths[2]
+  }
+  
+  // takes in the array of all the prs and returns the array with all closed&unmerged removed 
+  #getMergedPulls = () => {
+    let allPrs = this.pulls
+    let allMergedPrs = []
+    for (let i = 0; i < allPrs.length; i++) {
+      const mergeDate = allPrs[i].merged_at
+      if (mergeDate === null) {
+        allPrs.splice(i, 1)
+        i--
+      } else {
+        allMergedPrs.push(allPrs[i])
+      }
+    }
+    return allMergedPrs
+  }
+  
+  // takes in the merged Prs and returns an array of all the dates of each merge
+  #getPrMergeDates = () => {
+    let mergedPrs = this.mergedPulls
+    let allDates = []
+    for (let i = 0; i < mergedPrs.length; i++) {
+      const date = mergedPrs[i].merged_at.slice(0, 10)
+      allDates.push(date) 
+    }
+    return allDates
+  }
+
+  // takes in arr of prs that have been merged, returns names of all reviewers
+  #getAdminObjects = () => {
+    let allPrs = this.mergedPulls
+    let allReviewers = []
+    for (let i = 0; i < allPrs.length; i++) {
+      let assignee = allPrs[i].assignee
+      let assignees = allPrs[i].assignees
+      let reviewers = allPrs[i].requested_reviewers
+      let teams = allPrs[i].requested_teams 
+      
+      if (assignee !== null) {
+        allReviewers.push([assignee])
+      }
+      if (assignees.length !== 0) {
+        allReviewers.push(assignees)
+      }
+      if (reviewers.length !== 0) {
+        allReviewers.push(reviewers)
+      }
+      if (teams.length !== 0) {
+        allReviewers.push(teams)
+      }
+    }
+    return allReviewers.flat()
+  }
+  
+  // Takes in array of reviewers objects, repo owner, user admin input and returns all unique reviewers usernames
+  getReviewersNames = () => {
+    const allReviewers = this.adminsJson
+    const allExtras = this.userAdmins
+    let allRevUsernames = []
+
+    for (let i = 0; i < allReviewers.length; i++) {
+      const username = allReviewers[i].login
+      let nameExists = allRevUsernames.includes(username)
+      if (nameExists == false) {
+        allRevUsernames.push(username) 
+      }
+    }
+    
+    for (let i = 0; i < allExtras.length; i++) {
+      const username = allExtras[i] 
+      let nameExists = allRevUsernames.includes(username)
+      if (nameExists == false) {
+        allRevUsernames.push(username)
+      } 
+    }
+
+    if (allRevUsernames.includes(this.repoOwner) === false) {
+      allRevUsernames.push(this.repoOwner)
+    }
+    
+    return allRevUsernames
+  }
+  
+  // takes in an array of dates from merged prs and returns an object of key/value {date: how-many-merges}
+  getPrMergeFrequency = () => {
+    let mergeDates = this.mergeDates 
+    let frequencyObj = {}
+    for (let i = 0; i < mergeDates.length; i++) {
+      let date = mergeDates[i]
+      let doesExist = frequencyObj.hasOwnProperty(date)
+      if (doesExist) {
+        frequencyObj[date] = frequencyObj[date] + 1
+      } else {
+        frequencyObj[date] = 1
+      }
+    }
+    return JSON.stringify(frequencyObj)
+  }
+
+  // Uses the array of all PRs and array of Names to return the number of PRs that are from reviewers
+  getReviewersPullPercent = () => {
+    const mergedPrs = this.mergedPulls
+    const allNamesArr = this.getReviewersNames()
+    let totalRevsPrs = 0
+    for (let i = 0; i < mergedPrs.length; i++) {
+      let username = mergedPrs[i].user.login 
+      if (allNamesArr.includes(username)) {
+        totalRevsPrs += 1
+      }
+    }
+    return (Math.round((totalRevsPrs / mergedPrs.length) * 100))
+  }
+}
+
+module.exports = ClosedPr
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const cookie = require('cookie')
 const MakeClosedUrlsArr = require('./githubAuth')
 const FetchClosedPrs = require('./apiFetch')
-const ClosedParser = require('./closedPrParse')
+const ClosedPr = require('./closedPr')
 
 const server = express()
 const port = 3000
@@ -14,20 +14,27 @@ server.use(bodyParser.urlencoded({ extended: true }))
 
 server.get('/results', (req, res) => {
   const cookies = cookie.parse(req.headers.cookie || '');
-  res.send(cookies.value + '% --- this is the value')
+  res.send(cookies.value +'%' + '<br>' + cookies.freq + '<br>' + cookies.names)
 })
 
 server.post('/', async (req, res) => {
   const closedUrls = await MakeClosedUrlsArr(await req.body.url)
   if (closedUrls.length === 0) {
     res.redirect('/')
-    res.end()
     return
   }
   const closedPrData = await FetchClosedPrs(closedUrls)
-  const allReviewersPrs = ClosedParser(closedPrData, closedUrls, await req.body.logins) 
-  const revPercent = cookie.serialize('value', allReviewersPrs, { maxAge: 5 });
-  res.setHeader('Set-Cookie', revPercent)
+  const newClosedPr = new ClosedPr(closedPrData, closedUrls, await req.body.logins)
+
+  let freq = newClosedPr.getPrMergeFrequency()
+  let pullPerc = newClosedPr.getReviewersPullPercent()
+  let names = newClosedPr.getReviewersNames()
+
+  const revPercent = cookie.serialize('value', pullPerc, {maxAge: 5});
+  const revFreq = cookie.serialize('freq', freq, {maxAge: 5});
+  const revNames = cookie.serialize('names', names, {maxAge: 5});
+
+  res.setHeader('Set-Cookie', [revPercent, revFreq, revNames])
   res.redirect('/results')
 })
 
